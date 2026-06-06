0155c123043a293694f30425277b826775796511
sabin2002 <2002sabinshrestha@gmail.com>
2026-06-07 00:10:00 +0900

    Add SMTP debug logs

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -5,6 +5,7 @@ const sendEmail = async (to, subject, text) => {
   console.log("SMTP_HOST:", process.env.SMTP_HOST);
   console.log("SMTP_PORT:", process.env.SMTP_PORT);
   console.log("SMTP_USER:", process.env.SMTP_USER);
+  console.log("SMTP_PASS exists:", !!process.env.SMTP_PASS);
 
   const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
