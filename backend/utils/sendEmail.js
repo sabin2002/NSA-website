d672cb4de3e01861f63da52c6f1fb96fda01b14a
sabin2002 <2002sabinshrestha@gmail.com>
2026-06-07 00:05:51 +0900

    Add SMTP debug logs

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,6 +1,11 @@
 const nodemailer = require("nodemailer");
 
 const sendEmail = async (to, subject, text) => {
+
+  console.log("SMTP_HOST:", process.env.SMTP_HOST);
+  console.log("SMTP_PORT:", process.env.SMTP_PORT);
+  console.log("SMTP_USER:", process.env.SMTP_USER);
+
   const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: Number(process.env.SMTP_PORT),
@@ -12,7 +17,7 @@ const sendEmail = async (to, subject, text) => {
   });
 
   await transporter.sendMail({
-    from: `"NSA Website" <${process.env.SMTP_USER}>`,
+    from: process.env.SMTP_USER,
     to,
     subject,
     text,
