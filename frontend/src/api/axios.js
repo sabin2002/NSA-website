a2322e76a2c88db07bd2197181bc4995754b854c
sabin2002 <2002sabinshrestha@gmail.com>
2026-06-06 22:26:35 +0900

    Update API URL for production

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://nsa-website-production.up.railway.app/api",
 });
 
 export default API;
